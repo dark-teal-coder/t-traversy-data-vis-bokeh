24c8316852ba2ef7a1f035abdc95ec32d98e3167
Preesa Saewong <ree.wong.career@gmail.com>
2022-10-08 17:45:50 -0400

    Add comments

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-## 
+## `figure` to create plots 
+## `output_file` to name the HTML file to generate
+## 'show' to generate and output HTML files 
 from bokeh.plotting import figure, output_file, show 
 
+## Data 
 x = [1, 2, 3, 4, 5]
 y = [4, 6, 2, 4, 3]
 
+## Name the output HTML file
 output_file('index.html')
 
-# Add plot 
+## Add a plot 
 p = figure(
 	title = 'Simple Example', 
 	x_axis_label = 'X Axis', 
 	y_axis_label = 'Y Axis'
 )
 
-# Render glyph
+## Render glyph
+## Add a line graph
 p.line(x, y, legend='Test', line_width=2)
 
-# Show results
+## Show the plot
 show(p)
