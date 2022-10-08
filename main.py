c22cefeef8840d82e1b5f28a21175e58d4f16fe9
Preesa Saewong <ree.wong.career@gmail.com>
2022-10-08 18:15:52 -0400

    Change line graph to H-bar in main.py

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 ## `figure` to create plots
 ## `output_file` to name the HTML file to generate
 ## 'show' to generate and output HTML files
+from turtle import right
 from bokeh.plotting import figure, output_file, show
+import pandas as pd
 
 ## Data
-x = [1, 2, 3, 4, 5]
-y = [4, 6, 2, 4, 3]
+# x = [1, 2, 3, 4, 5]
+# y = [4, 6, 2, 4, 3]
+## Read in CSV data file with Pandas
+df = pd.read_csv('cars.csv')
+
+car = df['Car']
+hp = df['Horsepower']
 
 ## Name the output HTML file
 output_file('index.html')
 
 ## Add a plot
 p = figure(
-	title = 'Bokeh Line Graph Test',
-	x_axis_label = 'X Axis',
-	y_axis_label = 'Y Axis'
+	y_range=car,
+	plot_width=800,
+	plot_height=600,
+	title = 'Cars with Top Horsepower',
+	x_axis_label = 'Horsepower',
+	# y_axis_label = 'Y Axis',
+	## No tool displayed
+	tools=''
 )
 
 ## Render glyph
 ## Add a line graph
-p.line(x, y, legend='Test', line_width=2)
+# p.line(x, y, legend='Test', line_width=2)
+## Add a horizontal bar chart
+p.hbar(
+	y=car,
+	right=hp,
+	left=0,
+	height=0.4,
+	color='green',
+	fill_alpha=0.5
+	)
 
 ## Show the plot
 show(p)
